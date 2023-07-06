51ba23f513e3c874292a3ca7879e2c56d5842233
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-07-06 10:54:48 -0500

    Fixing login route again.

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,7 +5,7 @@ async function signupFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/user', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
