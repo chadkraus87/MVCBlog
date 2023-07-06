229b6eb8c3586ddc5843b6167a819a071569a3f1
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-07-06 15:45:20 -0500

    Fixed login GET route again.

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/user', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -14,9 +14,11 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert('Failed to log in');
+      alert(response.statusText);
     }
   }
 };
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
